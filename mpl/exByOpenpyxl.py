b9836aff4424a6a4601fb900c2df2da649114e14
Lem <leparskii.rd@phystech.edu>
2021-06-28 14:28:36 +0500

    28.06.2021

--- mpl/exByOpenpyxl.py
+++ mpl/exByOpenpyxl.py
@@ -11,23 +11,23 @@ x = list()
 y = list()
 ws = wb['Sheet1']
 for cell in  ws['A']:
-	x.append(cell.value)
+	x.append(float(cell.value))
 for cell in  ws['B']:
-	y.append(cell.value)
+	y.append(float(cell.value))
 
 # настраиваем детали отрисовки графиков
 plt.figure(figsize=(8, 6))
 
-plt.xlabel("t, c")
-plt.ylabel("P, 10^-4 Torr")
+plt.xlabel("l, см")
+plt.ylabel("P, Па")
 
 plt.autoscale(tight=True)
 
 # рисуем исходные точки
-plt.errorbar(x, y, yerr=0.05, xerr=0.5, fmt='.', ecolor='red', color='red')
+plt.errorbar(x, y, yerr=2, xerr=0.5, fmt='.', ecolor='red', color='red')
 
 # аргументы для построения графиков моделей: исходный интервал + 60 дней
-fx = sp.linspace(x[0], x[len(x) - 1], 100)
+fx = sp.linspace(x[0]-10, x[len(x)-1]+30, 100)
 
     # получаем параметры модели для полинома степени 1
 fp, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
@@ -36,28 +36,33 @@ fp, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
 f = sp.poly1d(fp)
 print(f)
 
+    # рисуем график модельной функции
+#plt.plot(fx, f(fx), linewidth=2)
+
+
+
 
 avgx = 0
 avgy = 0
 avgx2 = 0
 avgy2 = 0
 avgxy = 0
-for i in range(len(x)):
+for i in range(3):
 	avgx += x[i]
 	avgy += y[i]
 	avgx2 += x[i] * x[i]
 	avgy2 += y[i] * y[i]
 	avgxy += x[i] * y[i]
 
-avgx = avgx/len(x)
-avgy = avgy/len(x)
-avgx2 = avgx2/len(x)
-avgy2 = avgy2/len(x)
-avgxy = avgxy/len(x)
+avgx = avgx/6
+avgy = avgy/6
+avgx2 = avgx2/6
+avgy2 = avgy2/6
+avgxy = avgxy/6
 
 koef = (avgxy - avgx*avgy)/(avgx2 - avgx*avgx)
 
-sigma = np.sqrt((avgy2 - avgy*avgy)/(avgx2 - avgx*avgx) - koef*koef)/np.sqrt(len(x))
+sigma = np.sqrt((avgy2 - avgy*avgy)/(avgx2 - avgx*avgx) - koef*koef)/np.sqrt(6)
 
 print("Коэффициент = ")
 print(koef)
@@ -65,8 +70,7 @@ print(koef)
 print("Погрешность = ")
 print(sigma)
 
-    # рисуем график модельной функции
-plt.plot(fx, f(fx), linewidth=2)
+
 
 plt.grid()
 plt.savefig('data.png', dpi=50)
